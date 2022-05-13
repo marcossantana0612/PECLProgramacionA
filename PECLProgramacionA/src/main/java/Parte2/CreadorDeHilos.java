12224e18ea94a15324627ef8cdf01db2390495ef
LuciaGilDiaz <94480356+LuciaGilDiaz@users.noreply.github.com>
2022-05-13 14:05:43 +0200

    Código terminado y comentado

--- PECLProgramacionA/src/main/java/Parte2/CreadorDeHilos.java
+++ PECLProgramacionA/src/main/java/Parte2/CreadorDeHilos.java
@@ -17,24 +17,28 @@ public class CreadorDeHilos extends Thread {
 
     InterfazCampamento c;
     Parar paro;
-
+    
+    //Constructor
     public CreadorDeHilos(InterfazCampamento c, Parar paro) {
         this.c = c;
         this.paro = paro;
     }
-
+    
+    //Metodo run()
     public void run() {
         try {
             Campamento campamento = new Campamento(c);
             Servidor server = new Servidor(campamento);
             server.start();
 
+            //Asignacion de IDs para los monitores
             for (int i = 0; i < 4; i++) {
                 Monitor monitor = new Monitor("M" + i, campamento, paro);
                 monitor.start();
 
             }
 
+            //Asignacion de IDs para los ninios
             for (int i = 0; i < 20000; i++) {
                 Ninio ninio = new Ninio("N" + i, campamento, paro);
                 ninio.start();
